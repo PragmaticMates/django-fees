1dbd67783584873fcf75f58344304065d5968fb8
eriktelepovsky <erik.telepovsky@gmail.com>
2023-02-28 04:30:15 +0100

    added distinct period durations into package list view

--- fees/views.py
+++ fees/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import ListView, DetailView
 
-from fees.models import Package, Plan
+from fees.models import Package, Plan, Pricing
 
 
 class PackageListView(ListView):
@@ -8,6 +8,11 @@ class PackageListView(ListView):
 
     def get_queryset(self):
         return super().get_queryset().visible()
+
+    def get_context_data(self, *args, **kwargs):
+        context_data = super().get_context_data(*args, **kwargs)
+        context_data.update({'distinct_period_durations': Pricing.distinct_period_durations()})
+        return context_data
 
 
 class PlanDetailView(DetailView):
