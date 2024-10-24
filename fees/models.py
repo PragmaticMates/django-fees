a7e5e169e411e7e1d5b6316f211103c3bafadc8f
michalsoltys <michael.soltys8@gmail.com>
2024-10-24 15:29:12 +0200

    new plan validation - correction

--- fees/models.py
+++ fees/models.py
@@ -406,7 +406,7 @@ class Plan(models.Model):
         return "%s [%s]" % (self.purchaser, self.package)
 
     def clean(self):
-        existing_plans_with_package = Plan.objects.filter(purchaser=self.purchaser, package=self.package).active()
+        existing_plans_with_package = Plan.objects.filter(purchaser=self.purchaser, package=self.package).active().exclude(pk=self.pk)
 
         if existing_plans_with_package.exists():
             raise ValidationError(_("There is already an active plan with this package"))
